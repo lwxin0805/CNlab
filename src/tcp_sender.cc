10d372d86258918a983557ba403cbe40a0484191
lwxin0805 <3090658960@qq.com>
2024-12-05 12:10:34 +0800

    lab3

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -5,38 +5,131 @@ using namespace std;
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  // Your code here.
-  return {};
+  return bytes_in_flight_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  // Your code here.
-  return {};
+  return retransmission_count_;
+}
+
+uint64_t TCPSender::calculate_available_window() {
+    bool window_zero = receiver_window_size_ == 0;
+    uint64_t available_window;
+
+    if ((receiver_window_size_ + window_zero) > bytes_in_flight_) {
+        available_window = receiver_window_size_ + window_zero - bytes_in_flight_;
+    } else {
+        available_window = 0;
+    }
+
+    return available_window;
+}
+
+void TCPSender::send_message(TCPSenderMessage& msg, const TransmitFunction& transmit) {
+    msg.seqno = Wrap32::wrap(next_sequence_number_, zero_point_);
+    next_sequence_number_ += msg.sequence_length();
+    bytes_in_flight_ += msg.sequence_length();
+    outstanding_msg_.push_back(msg);
+    transmit(msg);
 }
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
-  // Your code here.
-  (void)transmit;
+  uint64_t available_window=calculate_available_window();
+  do {
+    if ( fin_flag_sent_ )
+      return;
+    uint64_t pay_load_size;
+    if ( reader().bytes_buffered() < TCPConfig::MAX_PAYLOAD_SIZE )
+    {
+      pay_load_size=reader().bytes_buffered();
+    }
+    else
+    {
+      pay_load_size=TCPConfig::MAX_PAYLOAD_SIZE;
+    }
+    uint64_t seq_size;
+    if ( available_window > pay_load_size + ( next_sequence_number_ == 0 ) )
+    {
+      seq_size=pay_load_size + ( next_sequence_number_ == 0 ) ;
+    }
+    else
+    {
+      seq_size=available_window;
+    }
+    pay_load_size = seq_size;
+    TCPSenderMessage msg = TCPSenderMessage();
+    if ( next_sequence_number_ == 0 ) {
+      msg.SYN = true;
+      pay_load_size--;
+    }
+    if ( reader().has_error() ) {
+      msg.RST = true;
+    }
+
+    while ( msg.payload.size() < pay_load_size ) {
+      string_view front_view = reader().peek();
+      uint64_t bytes_to_read = min( front_view.size(), pay_load_size - msg.payload.size() );
+      msg.payload += front_view.substr( 0, bytes_to_read );
+      input_.reader().pop( bytes_to_read );
+    }
+    if ( reader().is_finished() && seq_size < available_window ) {
+      msg.FIN = true;
+      seq_size++;
+      fin_flag_sent_= true;
+    }
+    if ( msg.sequence_length() == 0 )
+      return;
+    send_message(msg,transmit);
+    if ( expire_time_ == UINT64_MAX )
+      expire_time_ = elapsed_time_ + retransmission_timeout_interval_;
+   available_window=calculate_available_window();
+  } while ( reader().bytes_buffered() > 0 && available_window > 0 );
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
 {
-  // Your code here.
-  return {};
+  return { Wrap32::wrap( next_sequence_number_, zero_point_ ), false, string(), false, reader().has_error() };
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  if ( msg.RST )
+   {
+     writer().set_error();
+   }
+  receiver_window_size_ = msg.window_size;
+  if ( msg.ackno.has_value() ) {
+    uint64_t ack_from_recv = msg.ackno.value().unwrap( zero_point_, last_ack_);
+    if ( ack_from_recv > last_ack_ && ack_from_recv <= next_sequence_number_ ) {
+      last_ack_ = ack_from_recv;
+      retransmission_timeout_interval_= initial_RTO_ms_;
+      expire_time_ = elapsed_time_ + retransmission_timeout_interval_;
+      retransmission_count_ = 0;
+      while (!outstanding_msg_.empty() &&
+             outstanding_msg_.front().seqno.unwrap(zero_point_, last_ack_) + outstanding_msg_.front().sequence_length() <= last_ack_) {
+        bytes_in_flight_ -= outstanding_msg_.front().sequence_length();
+        outstanding_msg_.pop_front();
+      }
+      if ( outstanding_msg_.empty() ) {
+        expire_time_ = UINT64_MAX;
+      }
+    }
+  }
+
 }
 
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
-  (void)transmit;
-  (void)initial_RTO_ms_;
+  elapsed_time_ += ms_since_last_tick;
+  if ( expire_time_ != 0 && elapsed_time_ >= expire_time_ ) {
+    transmit( outstanding_msg_.front() );
+    if ( receiver_window_size_ != 0 ) {
+      retransmission_count_++;
+      retransmission_timeout_interval_ *= 2;
+    }
+    expire_time_ = elapsed_time_ + retransmission_timeout_interval_;
+  }
 }
+
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -16,9 +16,21 @@ class TCPSender
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
+    : input_( std::move( input ) )
+    , isn_( isn )
+    , initial_RTO_ms_( initial_RTO_ms )
+    , elapsed_time_( 0 )
+    , last_ack_( 0 )
+    , bytes_in_flight_( 0 )
+    , expire_time_( UINT64_MAX )
+    , retransmission_count_( 0 )
+    , receiver_window_size_( 1 )
+    , retransmission_timeout_interval_( initial_RTO_ms )
+    , next_sequence_number_( isn.unwrap( isn, 0 ) )
+    , zero_point_( isn )
+    , outstanding_msg_()
+    , fin_flag_sent_( false )
   {}
-
   /* Generate an empty TCPSenderMessage */
   TCPSenderMessage make_empty_message() const;
 
@@ -37,9 +49,12 @@ public:
   // Accessors
   uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
   uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
+
+  uint64_t calculate_available_window() ;
   Writer& writer() { return input_.writer(); }
   const Writer& writer() const { return input_.writer(); }
-
+  void send_message(TCPSenderMessage& msg, const TransmitFunction& transmit);
+  
   // Access input stream reader, but const-only (can't read from outside)
   const Reader& reader() const { return input_.reader(); }
 
@@ -48,4 +63,16 @@ private:
   ByteStream input_;
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  uint64_t elapsed_time_;
+  uint64_t last_ack_;
+  uint64_t bytes_in_flight_;
+  uint64_t expire_time_;
+  uint64_t retransmission_count_;
+  uint64_t receiver_window_size_;
+  uint64_t retransmission_timeout_interval_;
+  uint64_t next_sequence_number_;
+  Wrap32 zero_point_;
+  std::deque<TCPSenderMessage> outstanding_msg_;
+  bool fin_flag_sent_;
 };
+
