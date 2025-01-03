5f8011ca7d760c54abd099d23757e233eabdcd8f
lwxin0805 <3090658960@qq.com>
2025-01-03 21:35:07 +0800

    lab6

--- src/router.cc
+++ src/router.cc
@@ -1,30 +1,58 @@
-#include "router.hh"
 
-#include <iostream>
-#include <limits>
+#include "router.hh"
 
 using namespace std;
 
-// route_prefix: The "up-to-32-bit" IPv4 address prefix to match the datagram's destination address against
-// prefix_length: For this route to be applicable, how many high-order (most-significant) bits of
-//    the route_prefix will need to match the corresponding bits of the datagram's destination address?
-// next_hop: The IP address of the next hop. Will be empty if the network is directly attached to the router (in
-//    which case, the next hop address should be the datagram's final destination).
-// interface_num: The index of the interface to send the datagram out on.
-void Router::add_route( const uint32_t route_prefix,
+void Router::add_route(const uint32_t route_prefix,
                         const uint8_t prefix_length,
                         const optional<Address> next_hop,
-                        const size_t interface_num )
-{
-  cerr << "DEBUG: adding route " << Address::from_ipv4_numeric( route_prefix ).ip() << "/"
-       << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
+                        const size_t interface_num) {
+  cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/"
+       << static_cast<int>(prefix_length) << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)")
        << " on interface " << interface_num << "\n";
 
-  // Your code here.
+  router_table_.emplace(prefix_info(prefix_length, route_prefix), make_pair(interface_num, next_hop));
 }
 
-// Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
-void Router::route()
-{
-  // Your code here.
+void Router::route() {
+  for (auto &interface : _interfaces) {
+    auto &datagram_queue = interface->datagrams_received();
+
+    while (!datagram_queue.empty()) {
+      auto &datagram = datagram_queue.front();
+
+      if (datagram.header.ttl == 1 || datagram.header.ttl == 0) {
+        datagram_queue.pop();
+        continue;
+      }
+
+      datagram.header.ttl--;
+      datagram.header.compute_checksum();
+
+      uint32_t destination = datagram.header.dst;
+      auto best_match = router_table_.end();
+
+      for (auto it = router_table_.begin(); it != router_table_.end(); ++it) {
+        if (it->first.matches(destination)) {
+          best_match = it;
+          break;
+        }
+      }
+
+      if (best_match == router_table_.end()) {
+        datagram_queue.pop();
+        continue;
+      }
+
+      const auto &[interface_idx, next_hop] = best_match->second;
+      auto outgoing_interface = _interfaces.at(interface_idx);
+      if (next_hop.has_value()) {
+        outgoing_interface->send_datagram(datagram, next_hop.value());
+      } else {
+        outgoing_interface->send_datagram(datagram, Address::from_ipv4_numeric(datagram.header.dst));
+      }
+
+      datagram_queue.pop();
+    }
+  }
 }
--- src/router.hh
+++ src/router.hh
@@ -1,38 +1,56 @@
 #pragma once
 
+#include <map>
 #include <memory>
 #include <optional>
-
+#include <vector>
+#include <ranges>
+#include <iostream>
 #include "exception.hh"
 #include "network_interface.hh"
 
-// \brief A router that has multiple network interfaces and
-// performs longest-prefix-match routing between them.
-class Router
-{
+class Router {
 public:
   // Add an interface to the router
-  // \param[in] interface an already-constructed network interface
-  // \returns The index of the interface after it has been added to the router
-  size_t add_interface( std::shared_ptr<NetworkInterface> interface )
-  {
-    _interfaces.push_back( notnull( "add_interface", std::move( interface ) ) );
+  size_t add_interface(std::shared_ptr<NetworkInterface> interface) {
+    _interfaces.push_back(notnull("add_interface", std::move(interface)));
     return _interfaces.size() - 1;
   }
 
   // Access an interface by index
-  std::shared_ptr<NetworkInterface> interface( const size_t N ) { return _interfaces.at( N ); }
+  std::shared_ptr<NetworkInterface> interface(const size_t N) {
+    return _interfaces.at(N);
+  }
 
   // Add a route (a forwarding rule)
-  void add_route( uint32_t route_prefix,
-                  uint8_t prefix_length,
-                  std::optional<Address> next_hop,
-                  size_t interface_num );
+  void add_route(uint32_t route_prefix,
+                 uint8_t prefix_length,
+                 std::optional<Address> next_hop,
+                 size_t interface_num);
 
   // Route packets between the interfaces
   void route();
 
 private:
-  // The router's collection of network interfaces
-  std::vector<std::shared_ptr<NetworkInterface>> _interfaces {};
+  struct prefix_info {
+    uint32_t mask_;
+    uint32_t netID_;
+
+    explicit prefix_info(uint8_t prefix_length, uint32_t prefix)
+        : mask_{~(UINT32_MAX >> prefix_length)}, netID_{prefix & mask_} {}
+
+    bool matches(uint32_t dst_ip) const {
+      return (dst_ip & mask_) == netID_;
+    }
+
+    auto operator<=>(const prefix_info &other) const {
+      return other.mask_ != mask_ ? mask_ <=> other.mask_ : netID_ <=> other.netID_;
+    }
+  };
+
+  using routerT = std::multimap<prefix_info, std::pair<size_t, std::optional<Address>>, std::greater<prefix_info>>;
+  routerT::const_iterator find_export(uint32_t target_dst) const;
+
+  std::vector<std::shared_ptr<NetworkInterface>> _interfaces{};
+  routerT router_table_{};
 };
